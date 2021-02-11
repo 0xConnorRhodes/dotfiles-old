3f377a30a5a37211360db46acd5010b7b1fb4e33
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-10 20:48:21 -0600

    added spectrwm

---
+++ shared/stow_user/spectrwm/.config/spectrwm/autorun.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+xset b off &
+xset r rate 300 45 &
+xinput --set-prop "ETPS/2 Elantech Touchpad" "libinput Accel Speed" .3 #t460 setting was .2
+xinput --set-prop "ETPS/2 Elantech TrackPoint" "libinput Accel Speed" .3 #the t460 setting was .2
+xinput --set-prop "ETPS/2 Elantech TrackPoint" "libinput Natural Scrolling Enabled" 0 #disables reverse scrolling on trackpoint while keeping it set on touchpad per /usr/share/X11/xorg.conf.d/40-libinput.conf
+picom &
+exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+syncthing -no-browser -logfile=/home/connor/.cache/syncthing/syncthing.log &
+unclutter --timeout 1 --jitter 5 --ignore-scrolling &
+#exec ~/.local/config_nosync/bin/autorun_nosync.sh &
+#exec /home/connor/bin/init-keyboard.sh &
+sleep 6
+~/bin/notifications-on.sh
+~/bin/refbar
+#systemctl --user restart xcape #this used to be necessary, but an update made it unneccessary
---
+++ shared/stow_user/spectrwm/.config/spectrwm/baraction.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# baraction.sh script for spectrwm status bar
+
+## DISK
+hdd() {
+  hdd="$(df -h | awk 'NR==4{print $4}')"
+  echo -e "$hdd"
+}
+
+## VOLUME
+vol() {
+    vol=`amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }' | sed 's/on:/ /g' | sed 's/off:/ /g'`
+    echo -e "$vol"
+}
+
+## BATTERY
+bat() {
+    cap1="$(cat /sys/class/power_supply/BAT0/capacity)"
+    echo "$cap1"
+}
+
+## INTERNET
+int() {
+    grep -q "down" /sys/class/net/w*/operstate && wifiicon="📡" ||
+    	wifiicon="$(grep "^\s*w" /proc/net/wireless | awk '{ print "", int($3 * 100 / 70) "%" }')"
+    
+    lukecmd= printf "%s %s\n" "$wifiicon" "$(sed "s/down/❎/;s/up/🌐/" /sys/class/net/e*/operstate)" | awk '{print $1}'
+    
+    echo $lukecmd
+}
+
+
+## BLUETOOTH
+btooth() {
+    blt=$(bluetoothctl info | grep "Connected" | sed "s/Connected: yes//")
+    echo $blt
+}
+
+## WEATHER
+wtr() {
+	wtr2=$()
+	echo $wtr2
+}
+
+
+## work email
+#wmail() {
+#	wmail2=$(~/.config/mailnotifications/workmailnotifications.sh)
+#	echo $wmail2
+#}
+
+## personal email
+#pmail() {
+#	pmail2=$(~/.config/mailnotifications/personalmailnotifications.sh)
+#	echo $pmail2
+#}
+
+SLEEP_SEC=120
+#loops forever outputting a line every SLEEP_SEC secs
+
+# It seems that we are limited to how many characters can be displayed via
+# the baraction script output. And the the markup tags count in that limit.
+# So I would love to add more functions to this script but it makes the 
+# echo output too long to display correctly.
+while :; do
+	#echo "+@fg=1;  $(pmail) | +@fg=0;  $(hdd) | +@fg=0;  +@fn=0; $(vol) +@fg=0; |   $(bat)% |  $(btooth) |  $(int) |"
+	echo "+@fg=1; +@fg=0;  $(hdd) | +@fg=0;  +@fn=0;   $(bat)% |"
+	sleep $SLEEP_SEC
+done
