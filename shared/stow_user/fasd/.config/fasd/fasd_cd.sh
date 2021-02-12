ffbbd54a0a5b2cbe377d3c9c26d5cca70eabc920
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-11 23:18:04 -0600

    added fasd

---
+++ shared/stow_user/fasd/.config/fasd/fasd_cd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+fasd_cd() {
+  if [ $# -le 1 ]; then
+    fasd "$@"
+  else
+    local _fasd_ret="$(fasd -e 'printf %s' "$@")"
+    [ -z "$_fasd_ret" ] && return
+    [ -d "$_fasd_ret" ] && cd "$_fasd_ret" || printf %s\n "$_fasd_ret"
+  fi
+}
