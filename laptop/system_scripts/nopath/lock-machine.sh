edf2dc617d246297286403799b82fa3a457bfa04
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-15 14:23:26 -0600

    added screen lock script

---
+++ laptop/system_scripts/nopath/lock-machine.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Screen Lock script by Connor Rhodes (connorrhodes@gmail.com) 2021-02-15
+
+sleep 1
+xset dpms force off
+xinput --set-prop 14 "Device Enabled" "0" # this disables the mouse
+xinput --set-prop 13 "Device Enabled" "0" # this disables the trackpoint
+betterlockscreen --lock
+xinput --set-prop 14 "Device Enabled" "1"
+xinput --set-prop 13 "Device Enabled" "1"
