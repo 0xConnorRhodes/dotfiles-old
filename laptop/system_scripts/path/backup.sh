c4133e97c91d008f7002113a54b337b6cd8ad0dc
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-15 17:36:08 -0600

    added backintime backup config

--- laptop/system_scripts/path/backup.sh
+++ laptop/system_scripts/path/backup.sh
@@ -4,10 +4,10 @@ pacman -Qqm > /home/connor/.local/dotfiles/laptop/aur_packages.txt
 groups > /home/connor/.local/dotfiles_secret/laptop/user_groups.txt
 yay -Syu --noconfirm
 nvim --headless +PlugUpgrade +PlugUpdate +PlugInstall +qa
-#~/.emacs.d/bin/doom -y upgrade
+~/.emacs.d/bin/doom -y upgrade
 #/home/connor/.local/config_nosync/bin/cron/daily_atomic_notes_log.sh
 #sudo backintime backup
-#backintime backup
+backintime backup
 #shutdown -h 03:00
 #notify-send "The Computer will shut down at 3am"
 
