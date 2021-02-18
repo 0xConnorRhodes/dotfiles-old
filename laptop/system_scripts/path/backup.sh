f488b9dc51371163e851c36ffcd09c7d7b167bc5
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-18 06:23:49 -0600

    nightly backup autopush

--- laptop/system_scripts/path/backup.sh
+++ laptop/system_scripts/path/backup.sh
@@ -9,8 +9,34 @@ pip list --user > /home/connor/.local/dotfiles_secret/laptop/pip_user_packages.t
 pip list > /home/connor/.local/dotfiles_secret/laptop/pip_all_packages.txt
 groups > /home/connor/.local/dotfiles_secret/laptop/user_groups.txt
 yay -Syu --noconfirm
+~/.emacs.d/bin/doom -y sync
 ~/.emacs.d/bin/doom -y upgrade
 nvim --headless +PlugUpgrade +PlugUpdate +PlugInstall +qa
+
+# push git repos
+cd /home/connor/.local/dotfiles
+git pull
+git add .
+git commit -m "nightly backup autopush"
+git push
+
+cd /home/connor/.local/dotfiles_secret
+git pull
+git add .
+git commit -m "nightly backup autocommit"
+git push
+
+cd /home/connor/dox/notes
+git pull
+git add .
+git commit -m "nightly backup autocommit"
+git push
+
+cd /home/connor/.local/password_store
+pass git pull
+pass git add .
+pass git commit -m "nightly backup autocommit"
+pass git push
 
 # backup OS and user data
 sudo rm /tmp/backintime.lock
