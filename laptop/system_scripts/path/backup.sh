544ace8412b08eea75dad4184b6b780fc7b8afb2
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-18 06:39:40 -0600

    nightly backup autopush

--- laptop/system_scripts/path/backup.sh
+++ laptop/system_scripts/path/backup.sh
@@ -50,7 +50,7 @@ rm /tmp/backintime/backup
 # backup VMs using restic for block-level deduplication
 export RESTIC_REPOSITORY=sftp:gb:/mnt/pool/restic
 export RESTIC_PASSWORD=$(pass sysadmin/restic-laptop-backups-password | head -n1)
-#exec restic --limit-upload 20000 --verbose backup /home/connor/.local/virtual_machines/
+exec restic --limit-upload 20000 --verbose backup /home/connor/.local/virtual_machines/
 #exec restic forget --keep-daily 7 --keep-weekly 8 --keep-monthly 24 --keep-yearly 10 --prune
 echo "wemadeit"
 
