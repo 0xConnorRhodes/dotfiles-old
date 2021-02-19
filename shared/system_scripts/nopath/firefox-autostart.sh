1d028d6a33e26ad6702838e39cfbf42ea08a8217
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-19 12:48:01 -0600

    fixed firefox autostart.sh

--- shared/system_scripts/nopath/firefox-autostart.sh
+++ shared/system_scripts/nopath/firefox-autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script to allow spectrwm to autostart firefox with my custom theme
 
-env GTK_THEME="Redmond97 Firefox Solaris" /usr/lib/firefox/firefox %u
+env GTK_THEME="Redmond97 Firefox Solaris" firefox
