90a830606801c38471e5bce2a31a4de755881889
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-19 12:54:11 -0600

    updated colorpicker quick

--- shared/system_scripts/nopath/colorpicker-quick.sh
+++ shared/system_scripts/nopath/colorpicker-quick.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# color picker script to undo settings that interfere with the color picker and redo them after it's done
+# Connor Rhodes (connorrhodes.com)
+
+killall unclutter
+
+killall picom
+sed -i -E "s/^inactive-dim\ =\ /\#inactive-dim\ =\ /g" /home/connor/.config/picom/picom.conf
+picom &
 
 CHOICE=$(xcolor)
 
@@ -7,3 +15,9 @@ echo $CHOICE | xclip -selection secondary
 echo $CHOICE | xclip -selection clipboard
 
 notify-send $CHOICE
+
+
+sed -i -E "s/^\#inactive-dim\ =\ /inactive-dim\ =\ /g" /home/connor/.config/picom/picom.conf
+unclutter --timeout 1 --jitter 5 --ignore-scrolling &
+killall picom
+picom &
