8814f139be862e781b098004c17e9170bcca4c87
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-22 15:57:48 -0600

    commented lock machine because update switched device numbers

--- laptop/system_scripts/nopath/lock-machine.sh
+++ laptop/system_scripts/nopath/lock-machine.sh
@@ -4,10 +4,10 @@
 xset dpms force off
 sleep .5
 xset dpms force off
-xinput --set-prop 14 "Device Enabled" "0" # this disables the mouse
-xinput --set-prop 13 "Device Enabled" "0" # this disables the trackpoint
+#xinput --set-prop 14 "Device Enabled" "0" # this disables the mouse
+#xinput --set-prop 13 "Device Enabled" "0" # this disables the trackpoint
 pkill obsidian #kills obsidian, so I can edit dayplanner on phone without sync conflict
 lockargs="--timepos=x+105:h-70 --timestr=%H:%M" betterlockscreen -l
-xinput --set-prop 14 "Device Enabled" "1"
-xinput --set-prop 13 "Device Enabled" "1"
+#xinput --set-prop 14 "Device Enabled" "1"
+#xinput --set-prop 13 "Device Enabled" "1"
 obsidian &!
