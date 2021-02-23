a7d8e4d792b233a5667a2c5edada11d431b8c381
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-23 12:55:53 -0600

    improved dmenu launch script and added spell check script

--- laptop/system_scripts/nopath/dmenu-scriptlaunch.sh
+++ laptop/system_scripts/nopath/dmenu-scriptlaunch.sh
@@ -1,12 +1,29 @@
 #!/bin/zsh
-# laptop version of dmenu launcher script by Connor Rhodes (connorrhodes@gmail.com) 2021-02-15
+# laptop version of dmenu launcher script by Connor Rhodes (connorrhodes@gmail.com)
 
-NAME=$(ls \
+# show all dmenu script folder contents combined into one prompt
+SELECTION=$(ls \
 	~/.local/dotfiles/shared/system_scripts/dmenu-scripts/* \
-	~/.local/dotfiles/laptop/system_scripts/dmenu-scripts | xargs -n 1 basename | rofi -dmenu)
+	~/.local/dotfiles/laptop/system_scripts/dmenu-scripts | \
+	xargs -n 1 basename | \
+	dmenu -fn 'Hack:normal:pixelsize=32')
 
+# get the name of the file
+NAME=$(echo $SELECTION | awk '{print $1}')
+
+ARGS=$(echo $SELECTION | awk '{sub($1 FS,"")}7')
+
+
+# if NAME and ARGS are equal, there are no arguments. Unset that variable
+if [ "$ARGS" = "$NAME" ];then
+	unset ARGS
+fi
+
+# get the full path of the selected script for the final exec command
 FULLPATH=$(ls -d \
 	~/.local/dotfiles/shared/system_scripts/dmenu-scripts/* \
-	~/.local/dotfiles/laptop/system_scripts/dmenu-scripts/* | grep $NAME)
+	~/.local/dotfiles/laptop/system_scripts/dmenu-scripts/* \
+	| grep $NAME)
 
-exec urxvtc -e "$FULLPATH"
+# run the full path to the dmenu script with any arguments passed to it
+exec urxvtc -e "$FULLPATH" "$ARGS"
