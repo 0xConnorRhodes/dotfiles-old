d1b87268eaf58e540e1fe66c814f24a907ab30b5
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-24 17:06:34 -0600

    added firefox app mode config

--- laptop/system_scripts/nopath/rsync-config-backup.sh
+++ laptop/system_scripts/nopath/rsync-config-backup.sh
@@ -8,5 +8,8 @@ $RCMD /run/user/1000/connor-firefox-6v3rve6y.default-release/user.js \
 $RCMD /run/user/1000/connor-firefox-6v3rve6y.default-release/chrome/* \
 	/home/connor/.local/dotfiles/laptop/rsync/firefox/main_profile
 
-$RCMD /run/user/1000/connor-firefox-6v3rve6y.default-release/chrome/* \
+$RCMD /run/user/1000/connor-firefox-5perw3vc.App/user.js \
+	/home/connor/.local/dotfiles/laptop/rsync/firefox/app_profile
+
+$RCMD /run/user/1000/connor-firefox-5perw3vc.App/chrome/* \
 	/home/connor/.local/dotfiles/laptop/rsync/firefox/app_profile
