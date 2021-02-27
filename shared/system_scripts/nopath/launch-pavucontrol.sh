38e46fa35e05cf923fc4092e14e56c55e2b9d379
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-27 09:01:28 -0600

    integrated pavucontrol scratchpad into picom

--- shared/system_scripts/nopath/launch-pavucontrol.sh
+++ shared/system_scripts/nopath/launch-pavucontrol.sh
@@ -7,14 +7,15 @@ winname="Volume Control"
 if [ -z "$(wmctrl -l | grep "$winname")" ]
 # -z triggers if it is empty. so this is, if it is not running...
 
-then exec 
-# then run it
 
-sleep 3 #might require some fiddling. It's the time it takes to launch google chrome
+then exec /usr/bin/pavucontrol -t 2 & 
+# -t <num> specifies tab number to open on
+
+sleep 1 #might require some fiddling. It's the time it takes to launch google chrome
 
 winid=$(wmctrl -l | grep "$winname" | awk '{ print $1 }')
 
-exec ~/bin/set_wm_class $winid dynamic-scratchpad dictationIO
+exec ~/bin/set_wm_class $winid opaque-scratchpad Pavucontrol
 # set custom class and window names
 
 else 
