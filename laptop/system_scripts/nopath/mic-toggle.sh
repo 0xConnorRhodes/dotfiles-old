b820609d12fe4e75b4eba92e0dfa9d2075cb4afd
Connor Rhodes <cnnrrhodes@gmail.com>
2021-02-27 22:14:24 -0600

    nightly backup autocommit

--- laptop/system_scripts/nopath/mic-toggle.sh
+++ laptop/system_scripts/nopath/mic-toggle.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Toggle active pulse microphone and create a (self-replacing) notification to show the microphone status.
+# Connor Rhodes (connorrhodes.com)
+
 amixer -D pulse sset Capture toggle
 dunstify -r 19987 -t 2000 "mic" "$(amixer get Capture | awk 'NR==5' | awk '{ print $NF }')"
