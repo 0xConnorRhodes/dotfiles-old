8ec13f3f877651af8542ec0df345a758dab98645
Connor Rhodes <cnnrrhodes@gmail.com>
2021-03-03 16:59:04 -0600

    added google voice firefox LOF wrapper

---
+++ laptop/system_scripts/nopath/dotdesktop-wrapper-scripts/voice.desktop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script to launch or focus Pavucontrol and hide it if it is currently focused.
+# Connor Rhodes (connorrhodes.com)
+
+
+winname="Voice - Calls — Mozilla Firefox"
+
+winid=$(wmctrl -l | grep "$winname" | awk '{ print $1 }')
+
+if [ -z "$(wmctrl -l | grep "$winname")" ]
+# -z triggers if it is empty. so this is, if it is not running...
+
+
+then 
+	exec env GTK_THEME="Redmond97 Firefox Solaris" \
+		/usr/lib/firefox/firefox -P App \
+		--new-window https://voice.google.com/u/0/calls &
+	sleep 3 #might require some fiddling. It's the time it takes to launch google chrome
+
+	winid=$(wmctrl -l | grep "$winname" | awk '{ print $1 }')
+	wmctrl -r "$winname" -t $(wmctrl -d | grep \* | awk '{ print $1 }') 
+else 
+        winid=$(wmctrl -l | grep "$winname" | awk '{ print $1 }')
+
+	wmctrl -r "$winname" -t $(wmctrl -d | grep \* | awk '{ print $1 }') 
+fi
+
