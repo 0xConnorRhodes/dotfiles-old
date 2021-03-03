b14b38391263e637db3bcd1ba141d1e283d236e5
Connor Rhodes <cnnrrhodes@gmail.com>
2021-03-03 12:21:07 -0600

    added foldopen-lf

--- laptop/system_scripts/nopath/lock-machine.sh
+++ laptop/system_scripts/nopath/lock-machine.sh
@@ -8,4 +8,6 @@ xset s 5 5
 pkill obsidian #kills obsidian, so I can edit dayplanner on phone without sync conflict
 lockargs="--timepos=x+105:h-70 --timestr=%H:%M" betterlockscreen -l
 xset s 300 300
+# refresh top bar
+kill $(pstree -lp | grep -- -baraction.sh | sed "s/.*sleep//" | sed "s/(//" | sed "s/)//")
 obsidian &!
---
+++ shared/system_scripts/nopath/foldopen-lf.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+# Script presents an fzf list of my favorite directories and opens pcmanfm in the selected directory
+# new directories can be added to the favorites list with dmenu-scripts/add-foldopen
+# Connor Rhodes (connorrhodes.io)
+
+lf $(cat ~/.config/foldopen/foldopen_dirs.txt | fzf)
+exit
