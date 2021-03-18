6d5df42beaf5d3a115f24553f42cafc2a0a82f7c
Connor Rhodes <cnnrrhodes@gmail.com>
2021-03-17 21:14:15 -0500

    nightly backup autocommit

--- laptop/system_scripts/nopath/dotdesktop-wrapper-scripts/obs-studio.desktop.sh
+++ laptop/system_scripts/nopath/dotdesktop-wrapper-scripts/obs-studio.desktop.sh
@@ -6,9 +6,8 @@
 # Author: Connor Rhodes (connorrhodes.com)
 
 killall unclutter
-sleep 1
 hhpc -i 1 &
-sleep 1
+$HOME/.local/dotfiles/shared/system_scripts/nopath/notifications-off.sh
 
 obs
 
@@ -17,4 +16,12 @@ killall hhpc
 sleep 1
 #nohup unclutter --timeout 1 --jitter 5 --ignore-scrolling >/dev/null 2>&1
 #sleep 1
-unclutter --timeout 1 --jitter 5 --ignore-scrolling
+$HOME/.local/dotfiles/shared/system_scripts/nopath/notifications-on.sh
+sleep .5
+unclutter --timeout 1 --jitter 5 --ignore-scrolling &
+sleep .5
+
+#insert script to convert and process and upload etc. the final file (you will need to have OBS store in a cache directory first.
+#also, have the script start this ffmpeg conversion in a tmux window you can access and open a tmux session there (as your previous ffmpeg script
+
+
