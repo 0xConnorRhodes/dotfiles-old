6efd782c817e80fdfb5036ee6c911f9a29da8497
Connor Rhodes <cnnrrhodes@gmail.com>
2021-03-20 12:20:24 -0500

    finished custom screenshot script

--- laptop/system_scripts/nopath/screenshot-annotate.sh
+++ laptop/system_scripts/nopath/screenshot-annotate.sh
@@ -5,30 +5,12 @@
 
 killall unclutter
 
-CACHEDIR
+CACHEDIR=$HOME/.cache/screenshot-annotate
 
-trash-put /home/connor/.cache/tesseract-ocr/*
-rm /home/connor/.local/share/nvim/swap//%home%connor%.cache%tesseract-ocr%ocr.txt.swp
+trash-put $CACHEDIR/*
 
-import ~/.cache/tesseract-ocr/$(date +"%Y-%m-%d_%H-%M-%S").png
-
-#flameshot gui -p ~/.cache/tesseract-ocr/
+import $CACHEDIR/$(date +"%Y-%m-%d_%H-%M-%S").png
 
 unclutter --timeout 1 --jitter 5 --ignore-scrolling &
 
-#read NEWFILE < <(ls -t /home/connor/.cache/tesseract-ocr/*.png)
-
-cd /home/connor/.cache/tesseract-ocr
-
-/usr/bin/tesseract * ocr
-
-sed -i 's/\o14//g' ocr.txt
-
-sed -i '/^$/d' ocr.txt
-
-rm /home/connor/.local/share/nvim/swap//%home%connor%.cache%tesseract-ocr%ocr.txt.swp
-
-urxvtc -e nvim /home/connor/.cache/tesseract-ocr/ocr.txt
-
-xclip -r -selection clipboard -i /home/connor/.cache/tesseract-ocr/ocr.txt
-xclip -r -selection primary -i /home/connor/.cache/tesseract-ocr/ocr.txt
+ksnip -e $CACHEDIR/*
