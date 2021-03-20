fc7e4b8460662e97738d055e328088132249c240
Connor Rhodes <cnnrrhodes@gmail.com>
2021-03-20 12:27:44 -0500

    added copy to clipboard to screenshot annotate script

--- laptop/system_scripts/nopath/screenshot-annotate.sh
+++ laptop/system_scripts/nopath/screenshot-annotate.sh
@@ -3,14 +3,16 @@
 # gets around problem of multi-monitor screenshots in flameshot and 
 # Author: Connor Rhodes (connorrhodes.com)
 
-killall unclutter
-
 CACHEDIR=$HOME/.cache/screenshot-annotate
 
 trash-put $CACHEDIR/*
+
+killall unclutter ; killall hhpc
 
 import $CACHEDIR/$(date +"%Y-%m-%d_%H-%M-%S").png
 
 unclutter --timeout 1 --jitter 5 --ignore-scrolling &
 
 ksnip -e $CACHEDIR/*
+
+xclip -selection clipboard -target image/png -i $CACHEDIR/*
