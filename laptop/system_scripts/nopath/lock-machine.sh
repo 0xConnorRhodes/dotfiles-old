661292174a674b00ef8b8cbafc5d51bce756b925
Connor Rhodes <cnnrrhodes@gmail.com>
2021-03-21 21:44:51 -0500

    nightly backup autocommit

--- laptop/system_scripts/nopath/lock-machine.sh
+++ laptop/system_scripts/nopath/lock-machine.sh
@@ -5,7 +5,7 @@ xset dpms force off
 sleep .5
 xset dpms force off
 xset s 5 5
-pkill obsidian #kills obsidian, so I can edit dayplanner on phone without sync conflict
+#pkill obsidian #kills obsidian, so I can edit dayplanner on phone without sync conflict
 lockargs="--timepos=x+105:h-70 --timestr=%H:%M" betterlockscreen -l
 xset s 300 300
 # refresh top bar
