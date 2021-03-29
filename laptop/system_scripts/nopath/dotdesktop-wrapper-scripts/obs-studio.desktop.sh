d93c6c7d12bd9cdaa65769fe9fac0575b1a8254b
Connor Rhodes <cnnrrhodes@gmail.com>
2021-03-29 10:35:54 -0500

    updated obs wrapper script to change notes-search shortcut

--- laptop/system_scripts/nopath/dotdesktop-wrapper-scripts/obs-studio.desktop.sh
+++ laptop/system_scripts/nopath/dotdesktop-wrapper-scripts/obs-studio.desktop.sh
@@ -5,21 +5,44 @@
 # OBS_dotdesktop_name: com.obsproject.Studio.desktop
 # Author: Connor Rhodes (connorrhodes.com)
 
+# edit spectrwm to redefine from key presses
+## search only public notes in Obsidian
+sed -i \
+	-e "s/^\#program\[screencast-notes\]/program\[screencast-notes\]/g" \
+	-e "s/^\#bind\[screencast-notes\]/bind\[screencast-notes\]/g" \
+	$HOME/.local/dotfiles/laptop/stow_user/spectrwm/.config/spectrwm/spectrwm.conf
+
+## reset spectrwm to apply changes
+xdotool key Meta_L+shift+r
+
+
 killall unclutter
 #hhpc -i 1 &
 $HOME/.local/dotfiles/shared/system_scripts/nopath/notifications-off.sh
 
 obs
 
-sleep 1
+# reset spectrwm config to non-screencast keyboard shortcuts
+## search all notes in Obsidian
+sed -i \
+	-e "s/^program\[screencast-notes\]/\#program\[screencast-notes\]/g" \
+	-e "s/^bind\[screencast-notes\]/\#bind\[screencast-notes\]/g" \
+	$HOME/.local/dotfiles/laptop/stow_user/spectrwm/.config/spectrwm/spectrwm.conf
+
+## reset spectrwm to apply changes
+xdotool key Meta_L+shift+r
+
+#sleep 1
 killall hhpc
-sleep 1
+#sleep 1
 #nohup unclutter --timeout 1 --jitter 5 --ignore-scrolling >/dev/null 2>&1
 #sleep 1
 $HOME/.local/dotfiles/shared/system_scripts/nopath/notifications-on.sh
-sleep .5
+#sleep .5
 unclutter --timeout 1 --jitter 5 --ignore-scrolling &
-sleep .5
+#sleep .5
+
+
 
 #insert script to convert and process and upload etc. the final file (you will need to have OBS store in a cache directory first.
 #also, have the script start this ffmpeg conversion in a tmux window you can access and open a tmux session there (as your previous ffmpeg script
