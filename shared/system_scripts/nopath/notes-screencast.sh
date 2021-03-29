62fbfd3860718922b291edb6ccdeb81cc9cf1493
Connor Rhodes <cnnrrhodes@gmail.com>
2021-03-29 10:15:09 -0500

    notes-screencast add if open logic

--- shared/system_scripts/nopath/notes-screencast.sh
+++ shared/system_scripts/nopath/notes-screencast.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+# Script to present menu of public Engineer's Notes during a screencast and open selected
+# note in Obsidian.
+# Author: Connor Rhodes (connorrhodes.com)
+
 cd /home/connor/dox/notes/Engineers_Notes
 FILE=$(fd --extension md . | rofi -dmenu -i)
 
-#nvim "$FILE"
-xdg-open obsidian:///"/home/connor/dox/notes/Engineers_Notes/$FILE"
+# Only run the open command if there is a selection. Otherwise exit.
+if [ -z $FILE ];
+then
+    exit 0
+else
+    xdg-open obsidian:///"/home/connor/dox/notes/Engineers_Notes/$FILE"
+fi
